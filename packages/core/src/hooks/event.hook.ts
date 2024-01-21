5d9d860e58611846ceabce61e2d801ee8ee64d48
Jo du Plessis <jo@joduplessis.com>
2024-01-21 10:34:04 +0200

    Add custom event hook

--- packages/core/src/hooks/event.hook.ts
+++ packages/core/src/hooks/event.hook.ts
@@ -22,70 +22,10 @@ export const useElementEvent = (node, event, handler, passive = false) => {
     })
 }
 
-// editor
-
-export type EditorEventName = 'emoticon' | 'node'
-
-export const dispatchEditorEvent = (eventName: EditorEventName, data: any = {}) =>
+export const dispatchCustomEvent = (eventName: string, data: any = {}) =>
     documentObject.dispatchEvent(new CustomEvent(eventName, { detail: data }))
 
-export const useEditorEvent = (event: EditorEventName, handler, passive = false) => {
-    useEffect(() => {
-        documentObject.addEventListener(event, handler, passive)
-        return () => documentObject.removeEventListener(event, handler)
-    })
-}
-
-// drag
-
-export type DragEventName = 'ondrop' | 'onstart' | 'onend'
-
-export const dispatchDragEvent = (eventName: DragEventName, data: any = {}) =>
-    documentObject.dispatchEvent(new CustomEvent(eventName, { detail: data }))
-
-export const useDragEvent = (event: DragEventName, handler, passive = false) => {
-    useEffect(() => {
-        documentObject.addEventListener(event, handler, passive)
-        return () => documentObject.removeEventListener(event, handler)
-    })
-}
-
-// kanban
-
-export type KanbanEventName = 'columnvisible' | 'addcard' | 'addcolumn' | 'updateswimlane' | 'updatecolumn'
-
-export const dispatchKanbanEvent = (eventName: KanbanEventName, data: any = {}) =>
-    documentObject.dispatchEvent(new CustomEvent(eventName, { detail: data }))
-
-export const useKanbanEvent = (event: KanbanEventName, handler, passive = false) => {
-    useEffect(() => {
-        documentObject.addEventListener(event, handler, passive)
-        return () => documentObject.removeEventListener(event, handler)
-    })
-}
-
-// todo
-
-export type TodoEventName = 'sectionvisible' | 'addtask' | 'addsection' | 'updatesection'
-
-export const dispatchTodoEvent = (eventName: TodoEventName, data: any = {}) =>
-    documentObject.dispatchEvent(new CustomEvent(eventName, { detail: data }))
-
-export const useTodoEvent = (event: TodoEventName, handler, passive = false) => {
-    useEffect(() => {
-        documentObject.addEventListener(event, handler, passive)
-        return () => documentObject.removeEventListener(event, handler)
-    })
-}
-
-// data-grid
-
-export type DataGridEventName = 'row-selection' | 'clear-row-selection' | 'celledit' | 'celldelete'
-
-export const dispatchDataGridEvent = (eventName: DataGridEventName, data: any = {}) =>
-    documentObject.dispatchEvent(new CustomEvent(eventName, { detail: data }))
-
-export const useDataGridEvent = (event: DataGridEventName, handler, passive = false) => {
+export const useCustomEvent = (event: string, handler, passive = false) => {
     useEffect(() => {
         documentObject.addEventListener(event, handler, passive)
         return () => documentObject.removeEventListener(event, handler)
