3fed911d4b3aac4f039532d842566b739ba38ec7
Jo du Plessis <jo@joduplessis.com>
2024-01-22 06:43:14 +0200

    Add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ./publish design patch (or minor/major)
+# ./publish core patch (or minor/major)
+
+echo "Are you sure want to publish?"
+read -p "Want to continue? (y): " choice
+
+if [ $choice == "y" ]; then
+    cd ../packages/"$1"    
+
+    # patch: 1.0.0 -> 1.0.1
+    # minor: 1.0.1 -> 1.1.0
+    # major: 1.1.0 -> 2.0.0
+    npm version "$2"
+
+    env -i
+
+    git add -A
+    git commit -m "Bump $2 version"
+    git push origin $branch
+
+    echo "Done"
+else
+  echo "Bye"
+fi
+
+
+
+
+
+
+
