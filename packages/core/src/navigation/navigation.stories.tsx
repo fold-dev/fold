aaa7ccd9b7d178d6c927cf71c10439bc584c845d
Jo du Plessis <jo@joduplessis.com>
2024-01-25 14:16:30 +0200

    Remove underline when href is present

--- packages/core/src/navigation/navigation.stories.tsx
+++ packages/core/src/navigation/navigation.stories.tsx
@@ -30,7 +30,7 @@ export const Usage = () => (
         bgToken="surface-strong"
         p={20}>
         <Navigation width={300}>
-            <NavigationItem>Dashboard</NavigationItem>
+            <NavigationItem href="#">Dashboard</NavigationItem>
             <NavigationItem>Projects</NavigationItem>
             <NavigationItem>Products</NavigationItem>
             <NavigationItem>Analytics</NavigationItem>
