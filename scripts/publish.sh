104c1344c50149081f7ff735ef124571ef03443a
Jo du Plessis <jo@joduplessis.com>
2024-01-30 06:21:29 +0200

    Auto bump version, commiting & tagging

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-# ./publish design patch (or minor/major)
-# ./publish core patch (or minor/major)
+# this script auto incremenet the patch/minor/major versions
+# usage:
+# publish {patch|minor|major}
+# result:
+# patch: 1.0.0 -> 1.0.1
+# minor: 1.0.1 -> 1.1.0
+# major: 1.1.0 -> 2.0.0
 
 echo "Are you sure want to publish?"
 read -p "Want to continue? (y): " choice
 
 if [ $choice == "y" ]; then
-    cd "./packages/$1"
+    # current branch
+    branch=$(git branch | grep \* | cut -d ' ' -f2)
 
-    # patch: 1.0.0 -> 1.0.1
-    # minor: 1.0.1 -> 1.1.0
-    # major: 1.1.0 -> 2.0.0
-    npm version "$2"
+    # bump core version
+    cd "./packages/core"
+    npm version "$1"
 
-    env -i
+    # bump design version
+    cd -
+    cd "./packages/design"
+    npm version "$1"
 
+    # bump root version
+    cd -
+    npm version "$1"
+
+    # get the new version that was bumped
+    version_raw=$(npm pkg get version) 
+    version=$(echo "$version_raw" | tr -d '"')
+
+    # commit the new versions
     git add -A
-    git commit -m "Increment $1 $2 version"
+    git commit -m "Bump $1 version to $version"
     git push origin $branch
 
-    npm run build:prod
-    npm publish
+    # tag the repo so it deploys
+    git tag $version
+    git push origin $version
+    
+    # if we wanted to deploy from here
+    # npm run publish
 
     echo "Done"
 else
