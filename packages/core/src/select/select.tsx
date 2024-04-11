03dfc6a44e25710f42475accdc7b10e89c59cd39
Jo du Plessis <jo@joduplessis.com>
2024-04-11 17:00:18 +0200

    Remove subtle prop

--- packages/core/src/select/select.tsx
+++ packages/core/src/select/select.tsx
@@ -491,7 +491,6 @@ export type LabelSelectLabel = {
     id: string | number
     icon?: string
     color?: string
-    subtle?: boolean
     text: string
 }
 
@@ -593,7 +592,6 @@ export const LabelSelect = (props: LabelSelectProps) => {
                     <Pill
                         key={index}
                         color={label.color}
-                        subtle={label.subtle}
                         size={size}
                         suffix={
                             <IconLib
@@ -615,7 +613,6 @@ export type UserSelectUser = {
     id: string | number
     name: string
     image?: string
-    subtle?: boolean
 }
 
 export type UserSelectProps = {
@@ -712,7 +709,6 @@ export const UserSelect = (props: UserSelectProps) => {
                 users.map((user: UserSelectUser, index: number) => (
                     <Pill
                         key={index}
-                        subtle={user.subtle}
                         size={size}
                         prefix={
                             <Avatar
