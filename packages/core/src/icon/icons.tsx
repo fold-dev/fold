c36c4836da856ccd727a59394b42a8db89d2cda4
Jo du Plessis <jo@joduplessis.com>
2024-04-11 15:31:14 +0200

    Add audio & video icons

--- packages/core/src/icon/icons.tsx
+++ packages/core/src/icon/icons.tsx
@@ -819,3 +819,33 @@ export const FIStar = (props: any) => (
         />
     </svg>
 )
+
+export const FIVideo = (props: any) => (
+    <svg
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        {...props}>
+        <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" 
+        />
+    </svg>
+)
+
+export const FIAudio = (props: any) => (
+    <svg
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        {...props}>
+        <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" 
+        />
+    </svg>
+)
