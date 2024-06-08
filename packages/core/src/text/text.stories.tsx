1f3d398ada06f23daf2edf2cb42c233e9db077c6
Jo du Plessis <jo@joduplessis.com>
2024-06-08 20:07:57 +0200

    Fix spelling

--- packages/core/src/text/text.stories.tsx
+++ packages/core/src/text/text.stories.tsx
@@ -77,7 +77,7 @@ export const Variants = () => (
 
 // --
 
-export const ShowLessOrMOre = () => (
+export const ShowLessOrMore = () => (
     <LimitedText 
         html="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." 
     />
