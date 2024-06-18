43e80c36dcf520df100f7b5c8baffbc46ba0421c
Jo du Plessis <jo@joduplessis.com>
2024-06-18 15:19:48 +0200

    remove console.log

--- packages/core/src/drag/drag-manager.tsx
+++ packages/core/src/drag/drag-manager.tsx
@@ -115,8 +115,6 @@ export const DragManager = (props: DragManagerProps) => {
                 if (!!moveDirection && !shouldIndent && !shouldOutdent) {
                     const element = documentObject.elementFromPoint(mouseX, mouseY)
 
-                    console.log(element)
-
                     // only process valid elements (non-offscreen)
                     if (!element) return
                     if (!element.dataset) return
