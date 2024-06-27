ed89b25ff2987a2f5bd204523af8edf2120dc117
Jo du Plessis <jo@joduplessis.com>
2024-06-27 10:48:01 +0200

    prevent-scroll-prop

--- packages/core/src/hooks/focus.hook.ts
+++ packages/core/src/hooks/focus.hook.ts
@@ -22,31 +22,33 @@ export const useFocus = (focusable = FOCUSABLE, arrowNavigation = false) => {
     const focusableEls = useRef(null)
     const firstFocusableEl = useRef(null)
     const lastFocusableEl = useRef(null)
+    const shouldPreventScroll = useRef(false)
 
     const handleKeyDown = (e) => {
         let flag = false
         const { isUp, isDown, isTabNormal, isTabReverse, isTab, isShift } = getKey(e)
         const elements: any[] = Array.from(focusableEls.current)
+        const preventScroll = !!shouldPreventScroll.current
 
         if (isTab) {
             if (isShift && documentObject.activeElement === firstFocusableEl.current) {
                 e.preventDefault()
-                lastFocusableEl.current?.focus()
+                lastFocusableEl.current?.focus({ preventScroll })
             } 
             
             if (!isShift && documentObject.activeElement === lastFocusableEl.current) {
                 e.preventDefault()
-                firstFocusableEl.current?.focus()
+                firstFocusableEl.current?.focus({ preventScroll })
             }
         } 
     }
 
-    const trapFocus = (el) => {
+    const trapFocus = (el, preventScroll = false) => {
         containerRef.current = el
         focusableEls.current = containerRef.current.querySelectorAll(focusable.join(','))
         lastFocusableEl.current = focusableEls.current[focusableEls.current.length - 1]
         firstFocusableEl.current = focusableEls.current[0]
-        firstFocusableEl.current?.focus()
+        firstFocusableEl.current?.focus({ preventScroll })
         containerRef.current.addEventListener('keydown', handleKeyDown)
     }
 
